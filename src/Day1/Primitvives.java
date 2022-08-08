0587077f8d6fbb33924f2c2d93f0074721d032b2
Suna Kara <karacalisuna912@gmail.com>
2022-08-08 18:09:37 +0300

    new things added

---
+++ src/Day1/Primitvives.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class Primitvives {
+    public static void main(String[] args) {
+        int a=100;
+        double b = 2.5;
+        char c = 'C';
+        boolean d = true;
+
+
+        byte x = 100;
+        int y = x;
+
+        short z = (short) y;
+    }
+}
